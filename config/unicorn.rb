235b9ea6e7432a068d85e63b8b6319cda4f83004
soffolk <zlx.star@gmail.com>
2014-09-30 21:41:32 +0800

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,36 @@
+RAILS_ROOT = File.expand_path("../..", __FILE__)
+ENV["RACK_ENV"] = ENV["RAILS_ENV"] ||= "development"
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
+listen "#{RAILS_ROOT}/tmp/sockets/unicorn.sock"
+pid "#{RAILS_ROOT}/tmp/pids/unicorn.pid"
+stderr_path "#{RAILS_ROOT}/log/unicorn.err.log"
+stdout_path "#{RAILS_ROOT}/log/unicorn.out.log"
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = File.join RAILS_ROOT, "tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+  child_pid = server.config[:pid].sub(".pid", ".#{worker.nr}.pid")
+  system("echo #{Process.pid} > #{child_pid}")
 end
