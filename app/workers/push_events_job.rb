f47a5da6291ca487ddac41cbc05a74fc139f12d8
soffolk <zlx.star@gmail.com>
2014-09-30 20:34:23 +0800

    fix push-events-job worker

--- app/workers/push_events_job.rb
+++ app/workers/push_events_job.rb
@@ -4,13 +4,13 @@ class PushEventsJob
   sidekiq_options queue: :low, retry: 3
 
   def perform(payload)
-    payload = GitlabPushPayload.new(payload, gitlab).to_gitlab_payload
-    JobQueue.push(SmallBuildJob, payload.data) if payload
+    gitlab_payload = GitlabPushPayload.new(payload, gitlab).to_gitlab_payload
+    JobQueue.push(SmallBuildJob, gitlab_payload.data) if gitlab_payload
   end
 
   private
   def gitlab
-    GitlabApi.new(Rails.application.secrets.gitlab_ptivate_token)
+    GitlabApi.new(Rails.application.secrets.gitlab_private_token)
   end
 
 end
---
+++ spec/services/push_events_job_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe PushEventsJob do
+
+  it "should enqueue small_build_job" do
+    gitlab_payload = double("gitlabpayload", data: {})
+    GitlabPushPayload.stub_chain(:new, :to_gitlab_payload => gitlab_payload)
+    PushEventsJob.new.perform({})
+    expect(SmallBuildJob.jobs.count).to eq 1
+  end
+
+  it "should not enqueue small_build_job" do
+    GitlabPushPayload.stub_chain(:new, :to_gitlab_payload => nil)
+    PushEventsJob.new.perform({})
+    expect(SmallBuildJob.jobs.count).to eq 0
+  end
+
+end
