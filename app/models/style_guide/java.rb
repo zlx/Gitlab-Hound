3091c33fa7aadccdc79633427ee4bbe71629628c
soffolk <zlx.star@gmail.com>
2014-10-23 00:22:00 +0800

    add custom java config

--- app/models/style_guide/java.rb
+++ app/models/style_guide/java.rb
@@ -2,9 +2,18 @@ module StyleGuide
   class Java < Base
 
     def violations_in_file(file)
-      Jlint.lint(file.content, "").compact.map do |violation|
+      team.lint(file.content).compact.map do |violation|
         Violation.new(file, violation[1], violation[0])
       end
+    end
+
+    private
+    def config_content
+      repo_config.for(name)
+    end
+
+    def team
+      @team ||= Jlint.new(config_content)
     end
   end
 end
